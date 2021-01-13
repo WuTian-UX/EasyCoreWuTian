7789b9005057393f76593312c377dd901a1e08f6
王淇誉 <wangqylife@gmail.com>
2021-01-13 16:13:40 +0800

    修改模板

--- EasyCore.API/Controllers/Common/ErrorCatchAttribute.cs
+++ EasyCore.API/Controllers/Common/ErrorCatchAttribute.cs
@@ -2,7 +2,7 @@
  * 作者：WuTian
  * 版本号：v1.0
  * 本类主要用途及食用方式：
- * 捕捉控制器所有错误，格式化为ContentResult后返回前端
+ * 捕捉控制器所有错误，格式化为ContentResult后返回前端 
  *  -------------------------------------------------------------------------*/
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
